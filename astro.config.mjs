415330b4fd3ee3aeb64d35c1ea1da0e87e0bf28a
Andrey <codenomnom@users.noreply.github.com>
2026-02-03 21:07:04 +0200

    chore: initial commit
    boilerplate: astro-full-starter

---
+++ astro.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+
+import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://astro-full-starter.pages.dev/',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  integrations: [
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          es: 'es-ES'
+        }
+      }
+    }),
+    robotsTxt()
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true
+      }
+    }
+  }
+})
