bc1ce72c1ea36be708c758e586448265409d49f9
Andrey <codenomnom@users.noreply.github.com>
2026-02-11 09:19:06 +0200

    feat: custom fonts through astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config';
 
-import react from '@astrojs/react'
-import tailwindcss from '@tailwindcss/vite'
-import sitemap from '@astrojs/sitemap'
-import robotsTxt from 'astro-robots-txt'
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL,
+  experimental: {
+    // name: 'DM Serif Display',
+    // name: 'Source Serif Pro',
+    // name: 'Vollkorn',
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: 'Crimson Pro',
+        cssVariable: '--font-crimson-pro',
+        subsets: ['latin'],
+      },
+      {
+        provider: fontProviders.fontsource(),
+        name: 'Vollkorn',
+        cssVariable: '--font-vollkorn',
+        subsets: ['latin'],
+      },
+    ],
+  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
     routing: {
-      prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
   integrations: [
     react(),
@@ -24,17 +43,17 @@ export default defineConfig({
         locales: {
           en: 'en-US',
           es: 'es-ES'
-        }
-      }
+        },
+      },
     }),
-    robotsTxt()
+    robotsTxt(),
   ],
   vite: {
     plugins: [tailwindcss()],
     server: {
       watch: {
-        usePolling: true
-      }
-    }
-  }
-})
+        usePolling: true,
+      },
+    },
+  },
+});
